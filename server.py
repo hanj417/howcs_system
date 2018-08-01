0f1f754ae67c77a0d8a998088854c010451d4f88
hanjoon <hanj417@gmail.com>
2018-08-01 21:01:16 +0900

    alchemy dump 추가

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 
-from backend import app
+from backend import manager
 
 if __name__ == '__main__':
-    app.run()
+    manager.run()
+    #app.run()
     #app.run(host='0.0.0.0', port=5000, debug=False)
